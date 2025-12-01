5f9bfd69e5166984aec153ff9aebdda2f4a2fa84
yjyoon <yjyoon@wavve.com>
2025-12-01 22:33:47 +0900

    20251201

---
+++ financial-analyst/finacial_advisor/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py 
+# 파이썬 모듈로 인식되게 하기 위한 파일
+from . import agent
---
+++ financial-analyst/finacial_advisor/agent.py
@@ -0,0 +1,29 @@
+from google.adk.agents import Agent 
+from google.adk.models.lite_llm import LiteLlm
+
+MODEL = LiteLlm("openai/gpt-4o")
+
+def get_weather(city):
+    return f"The weather in {city} is 30 degress."
+
+def conver_units(degress: int):
+    return f"That is 40 farenheit"
+
+geo_agent = Agent(
+    name="GeoAgent",
+    instruction="You help the user with geo questions",
+    model=MODEL,
+    description="Transfer to this agent when you have a geo related question."
+)
+
+
+weather_agent = Agent(
+    name="WeatherAgent",
+    instruction="You help the user with weather related questions",
+    model=MODEL,
+    tools=[get_weather, conver_units],
+    sub_agents=[geo_agent],
+    description="Transfer to this agent when you have a weather related question."
+)
+# 반드시 root_agent
+root_agent = weather_agent
